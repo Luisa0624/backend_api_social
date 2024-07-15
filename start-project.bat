4a233b4b7c6bed6d86f8966618b8b43973b8f0a6
Luisa Maria Gil Garcia <luisamariagilgarcia24@gmail.com>
2024-07-15 11:36:54 -0500

    Se realiza backend de red social

---
+++ start-project.bat
@@ -0,0 +1,5 @@
+:: Inicia el servidor de Node.js
+start cmd.exe /k "cd /d C:\Users\Soporte\Documents\Bootcamp_FullStack\Actividades\Red Social\backend_api_social && npm run start:watch"
+
+::Ya no necesitamos iniciar mongod.exe localmente porque usamos MongoDB Atlas
+::start cmd.exe /k "cd /d C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath D:\ProjectsBootcamp\bdata-mongodb"
